ebca5343bd4a8aa0a48a0b7dd02aed5c737e3bda
Thomas Pucci <thomas.lb.pucci@gmail.com>
2022-10-05 13:48:03 +0200

    refactor: merge main and app

--- src/app.tsx
+++
@@ -1,6 +0,0 @@
-import "./style.css"
-import { JSX } from "preact"
-
-export function App(): JSX.Element {
-  return <> </>
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
-import { render } from "preact"
-import { App } from "./app"
+import { render, JSX } from "preact"
+import "./style.css"
+
+function App(): JSX.Element {
+  return <> </>
+}
 
 render(<App />, document.getElementById("app") as HTMLElement)
